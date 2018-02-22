7a99a22d2335abf38a1151ac71cb4ba033877ce3
Ishaan Varshney <ishaan.varshney@gmail.com>
2018-02-22 13:17:41 +1100

    :nail_care: dunno what happened

--- rec_will_fit.py
+++ rec_will_fit.py
@@ -1,13 +1,12 @@
 import math
 
 a, b = map(lambda x: float(x) / 2,
-           input('enter lengths of larger rec: ').split(' '))
-
+           input('enter dimensions of larger rec: ').split(' '))
 c, d = map(lambda x: float(x) / 2,
-           input('enter lengths of smaller rec: ').split(' '))
+           input('enter dimensions of smaller rec: ').split(' '))
 f = math.hypot(c, d)
+p = b - math.sqrt(math.pow(f, 2) - math.pow(a, 2))
+q = a - math.sqrt(math.pow(f, 2) - math.pow(b, 2))
 
-ans = math.pow(2 * d, 2) <= math.pow(
-    b - math.sqrt(math.pow(f, 2) - math.pow(a, 2)), 2) + math.pow(
-        a - math.sqrt(math.pow(f, 2) - math.pow(b, 2)), 2)
+ans = p > 0 and q > 0 and math.pow(2 * d, 2) <= math.pow(p, 2) + math.pow(q, 2)
 print(ans)
